487a231b16c1bcdaccb2d040bd782a9a6a20996d
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-05-05 18:51:48 -0400

    Add course route

---
+++ routes/course.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const courseCtrl = require('../controllers/courseCtrl');
+const { body } = require('express-validator');
+
+router.get('/', courseCtrl.getAllCourses);
+router.post("/", [body('title').notEmpty().trim().escape()], courseCtrl.saveCourse);
+
+module.exports = router;
