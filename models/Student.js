e50701a6666ec37fb92f7a534b51e51904df268b
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-05-05 18:50:05 -0400

    Add student model

---
+++ models/Student.js
@@ -0,0 +1,18 @@
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    familyName: {
+      type: String,
+    },
+    dob: {
+      type: Date,
+    },
+  },
+  { timestamps: false }
+);
+
+module.exports = mongoose.model("students", studentSchema);
